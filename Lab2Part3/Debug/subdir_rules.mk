57b1b9401e6c004eaeb744daa6b4b245150e22f2
Entov <gentov@wpi.edu>
2018-11-02 12:50:14 -0400

    Seem to have part 2 done, set up for part 3

---
+++ Lab2Part3/Debug/subdir_rules.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv7/tools/compiler/c6000_7.4.24/bin/cl6x" -mv6713 --abi=coffabi -g --include_path="R:/ECE4703_B18/Lab2Part3" --include_path="C:/ti/ccsv7/tools/compiler/c6000_7.4.24/include" --include_path="R:/ECE4703_B18/C6xCSL/include" --include_path="R:/ECE4703_B18/DSK6713/c6000/dsk6713/include" --diag_wrap=off --diag_warning=225 --display_error_number --mem_model:const=far --mem_model:data=far --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+vectors.obj: ../vectors.asm $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv7/tools/compiler/c6000_7.4.24/bin/cl6x" -mv6713 --abi=coffabi -g --include_path="R:/ECE4703_B18/Lab2Part3" --include_path="C:/ti/ccsv7/tools/compiler/c6000_7.4.24/include" --include_path="R:/ECE4703_B18/C6xCSL/include" --include_path="R:/ECE4703_B18/DSK6713/c6000/dsk6713/include" --diag_wrap=off --diag_warning=225 --display_error_number --mem_model:const=far --mem_model:data=far --preproc_with_compile --preproc_dependency="vectors.d" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
